d3cd829d366cdc6b017dc1b5a3c9bed8edd71e9c
Fred Rolland <frolland@redhat.com>
2020-01-13 17:43:21 +0200

    Improve CRD structure

--- api/v1alpha1/observatorium_types.go
+++ api/v1alpha1/observatorium_types.go
@@ -26,13 +26,31 @@ type ObservatoriumSpec struct {
 	Thanos ThanosSpec `json:"thanos"`
 }
 
-type ThanosReceiveController struct {
+type ReceiveController struct {
 	// Thanos receive controller Image name
 	Image *string `json:"image"`
 	// Tag describes the tag of Thanos receive controller to use.
 	Tag *string `json:"tag,omitempty"`
 	// Hashrings describes a list of Hashrings
 	Hashrings []*Hashring `json:"hashrings,omitempty"`
+}
+
+
+type ReceiveSpec struct {
+	Replicas *int32 `json:"replicas,omitempty"`
+	// Resources for Receive pods
+	Resources v1.ResourceRequirements `json:"resources,omitempty"`
+	// Receive Storage Class
+	StorageClass *string `json:"rstorageClass"`
+	// Receive PVC size
+	PVCSize *string `json:"pvcSize"`
+}
+
+
+type ThanosComponentSpec struct {
+	Replicas *int32 `json:"replicas,omitempty"`
+	// Resources for Querier pods
+	Resources v1.ResourceRequirements `json:"resources,omitempty"`
 }
 
 type Hashring struct {
@@ -48,28 +66,17 @@ type ThanosSpec struct {
 	Image *string `json:"image"`
 	// Tag of Thanos sidecar container image to be deployed.
 	Tag *string `json:"tag"`
+    // Thanos Receive Controller Spec
+	ReceiveControllerSpec ReceiveController `json:"receiveControllerSpec"`
+	// Thanos ReceiveSpec
+	Receive ReceiveSpec `json:"receive"`
+	// Thanos QuerierSpec
+	Querier ThanosComponentSpec `json:"querier"`
+	// Thanos StoreSpec
+	Store ThanosComponentSpec `json:"store"`
+	// Thanos CompactorSpec
+	Compactor ThanosComponentSpec `json:"compactor"`
 
-	ThanosReceiveControllerSpec ThanosReceiveController `json:"thanosReceiveControllerSpec"`
-	// Number of instances to deploy for a Thanos querier.
-	QuerierReplicas *int32 `json:"querierReplicas,omitempty"`
-	// Resources for Querier pods
-	QuerierResources v1.ResourceRequirements `json:"querierResources,omitempty"`
-	// Number of instances to deploy for a Thanos Store.
-	StoreReplicas *int32 `json:"storeReplicas,omitempty"`
-	// Resources for Store pods
-	StoreResources v1.ResourceRequirements `json:"storeResources,omitempty"`
-	// Number of instances to deploy for a Thanos Compactor.
-	CompactorReplicas *int32 `json:"compactorReplicas,omitempty"`
-	// Resources for Compactor pods
-	CompactorResources v1.ResourceRequirements `json:"compactorResources,omitempty"`
-	// Number of instances to deploy for a Thanos Receive.
-	ReceiveReplicas *int32 `json:"receiveReplicas,omitempty"`
-	// Resources for Receive pods
-	ReceiveResources v1.ResourceRequirements `json:"receiveResources,omitempty"`
-	// Receive Storage Class
-	ReceiveStorageClass *string `json:"receiveStorageClass"`
-	// Receive PVC size
-	ReceivePVCSize *string `json:"receivePvcSize"`
 	// Object Store Config Secret for Thanos
 	ObjectStoreConfigSecret *string `json:"objectStoreConfigSecret"`
 	// TODO: AWS secrets?
